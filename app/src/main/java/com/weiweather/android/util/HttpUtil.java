f8bdc2bd732ca9b952b19a2b1581fec118bd92ee
zhuyifei0 <zhuyifei0@foxmail.com>
2018-04-16 22:56:33 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/weiweather/android/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.weiweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by zhufei on 2018/4/16.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
