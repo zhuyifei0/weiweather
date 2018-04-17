ca725cad051d43a44161ac86877821c73aaf8c69
zhuyifei0 <zhuyifei0@foxmail.com>
2018-04-17 20:36:55 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/weiweather/android/WeatherActivity.java
+++ app/src/main/java/com/weiweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.weiweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.weiweather.android.gson.Forecast;
 import com.weiweather.android.gson.Weather;
+import com.weiweather.android.service.AutoUpdateService;
 import com.weiweather.android.util.HttpUtil;
 import com.weiweather.android.util.Utility;
 
@@ -241,6 +243,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
